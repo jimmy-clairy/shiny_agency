0e0af058f65c8e4fa21dd5cc8d7f262d67629166
Jimmy Clairy <jimcl@hotmail.fr>
2022-08-09 07:56:22 +0200

    end part 1

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Survey from './pages/Survey/Survey'
+import Results from './pages/Results/Results'
+import Freelances from './pages/Freelances/Freelances'
+import Header from './components/Header/Header'
+import Error from './components/Error/Error'
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+    div {
+        font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <GlobalStyle />
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/survey/:questionNumber" element={<Survey />} />
+      <Route path="/results" element={<Results />} />
+      <Route path="/freelances" element={<Freelances />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  </BrowserRouter>
+)
