50ad210e43a7642136599bf407967774489db862
Jimmy Clairy <jimcl@hotmail.fr>
2022-08-10 17:36:28 +0200

    P2C3 end

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,8 +1,9 @@
 import Card from '../../components/Card'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import { Loader } from '../../utils/style/Atoms'
+import { useFetch } from '../../utils/hooks'
 
 const CardsContainer = styled.div`
   display: flex;
@@ -33,34 +34,46 @@ const LoaderContainer = styled.div`
   font-size: 30px;
 `
 function Freelances() {
-  const [freelanceProfiles, setFreelanceProfiles] = useState([])
-  const [dataLoading, setDataLoading] = useState(false)
+  // const [freelanceProfiles, setFreelanceProfiles] = useState([])
+  // const [dataLoading, setDataLoading] = useState(false)
+  const { data, isLoading, error } = useFetch(
+    'http://localhost:8000/freelances'
+  )
 
-  useEffect(() => {
-    setDataLoading(true)
-    fetch('http://localhost:8000/freelances')
-      .then((res) => res.json())
-      .then(({ freelancersList }) => {
-        console.log(freelancersList)
-        setFreelanceProfiles(freelancersList)
-        setDataLoading(false)
-      })
-      .catch((error) => console.log(error))
-  }, [])
+  // const { freelanceProfiles } = data
+
+  const freelancersList = data?.freelancersList
+
+  // useEffect(() => {
+  //   setDataLoading(true)
+  //   fetch('http://localhost:8000/freelances')
+  //     .then((res) => res.json())
+  //     .then(({ freelancersList }) => {
+  //       console.log(freelancersList)
+  //       setFreelanceProfiles(freelancersList)
+  //       setDataLoading(false)
+  //     })
+  //     .catch((error) => console.log(error))
+  // }, [])
+
+  if (error) {
+    return <span>Oups il y a un problème</span>
+  }
+
   return (
     <div>
       <PageTitle>Trouvez votre prestataire</PageTitle>
       <PageSubtitle>
         Chez Shiny nous réunissons les meilleurs profils pour vous.
       </PageSubtitle>
-      {dataLoading ? (
+      {isLoading ? (
         <LoaderContainer>
           <Loader />
           Veuillez patienter
         </LoaderContainer>
       ) : (
         <CardsContainer>
-          {freelanceProfiles.map((profile) => (
+          {freelancersList.map((profile) => (
             <Card
               key={profile.id}
               label={profile.job}
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState, useContext } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 import { Loader } from '../../utils/style/Atoms'
 import { SurveyContext } from '../../utils/context'
+import { useFetch, useTheme } from '../../utils/hooks'
 
 const SurveyContainer = styled.div`
   display: flex;
@@ -14,25 +16,22 @@ const SurveyContainer = styled.div`
 const QuestionTitle = styled.h2`
   text-decoration: underline;
   text-decoration-color: ${colors.primary};
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const QuestionContent = styled.span`
   margin: 30px;
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
 `
 
 const LinkWrapper = styled.div`
   padding-top: 30px;
   & a {
-    color: black;
+    color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
   }
   & a:first-of-type {
     margin-right: 20px;
   }
-`
-const LoaderContainer = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 20px;
 `
 
 const ReplyBox = styled.button`
@@ -42,7 +41,9 @@ const ReplyBox = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${colors.backgroundLight};
+  background-color: ${({ theme }) =>
+    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
   border-radius: 30px;
   cursor: pointer;
   box-shadow: ${(props) =>
@@ -65,62 +66,49 @@ function Survey() {
   const questionNumberInt = parseInt(questionNumber)
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
   const nextQuestionNumber = questionNumberInt + 1
-  const [questions, setQuestions] = useState({})
-  const [dataLoading, setDataLoading] = useState(false)
-  const { answers, saveAnswers } = useContext(SurveyContext)
-  const [error, setError] = useState(false)
+  const { theme } = useTheme()
+
+  const { saveAnswers, answers } = useContext(SurveyContext)
 
   function saveReply(answer) {
     saveAnswers({ [questionNumber]: answer })
   }
-
-  // Appel API
-  useEffect(() => {
-    setDataLoading(true)
-    fetch('http://localhost:8000/survey')
-      .then((res) => res.json())
-      .then(({ surveyData }) => {
-        console.log(surveyData)
-        setQuestions(surveyData)
-        setDataLoading(false)
-      })
-      .catch((error) => {
-        console.log(error)
-        setError(true)
-      })
-  }, [])
+  const { data, isLoading, error } = useFetch(`http://localhost:8000/survey`)
+  const surveyData = data?.surveyData
 
   if (error) {
-    return <span>Oups il y a un problème</span>
+    return <span>Oups il y a eu un problème</span>
   }
 
   return (
     <SurveyContainer>
-      <QuestionTitle>Question {questionNumber}</QuestionTitle>
-      {dataLoading ? (
-        <LoaderContainer>
-          <Loader />
-        </LoaderContainer>
+      <QuestionTitle theme={theme}>Question {questionNumber}</QuestionTitle>
+      {isLoading ? (
+        <Loader />
       ) : (
-        <QuestionContent>{questions[questionNumber]}</QuestionContent>
+        <QuestionContent theme={theme}>
+          {surveyData[questionNumber]}
+        </QuestionContent>
       )}
       <ReplyWrapper>
         <ReplyBox
           onClick={() => saveReply(true)}
           isSelected={answers[questionNumber] === true}
+          theme={theme}
         >
           Oui
         </ReplyBox>
         <ReplyBox
           onClick={() => saveReply(false)}
           isSelected={answers[questionNumber] === false}
+          theme={theme}
         >
           Non
         </ReplyBox>
       </ReplyWrapper>
-      <LinkWrapper>
+      <LinkWrapper theme={theme}>
         <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-        {questions[questionNumberInt + 1] ? (
+        {surveyData && surveyData[questionNumberInt + 1] ? (
           <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
         ) : (
           <Link to="/results">Résultats</Link>
